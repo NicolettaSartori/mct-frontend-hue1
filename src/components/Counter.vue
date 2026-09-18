9dae99204ff2ec20ffac6278b4602750a52c37b5
Nicoletta <nicoletta.s.s@outlook.com>
2026-09-18 16:16:01 +0200

    Initial commit

---
+++ src/components/Counter.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import {computed, ref} from "vue";
+
+  const counter = ref(0);
+  const doubleCounter = computed(() => counter.value * 2);
+
+  const increment = () => {
+    counter.value++;
+  }
+  const decrement = () => {
+    if (counter.value >= 1)
+      counter.value--;
+  }
+</script>
+
+<template>
+  <h1>A - Counter</h1>
+  <p>{{ counter }}</p>
+  <p>Double counter: {{ doubleCounter }}</p>
+  <p>
+    <button @click="increment">Aufi</button>
+    <button @click="decrement">Owi</button>
+  </p>
+</template>
+
+<style scoped>
+
+</style>
